3ede192766e6587e26a95e2e5e65cc7b8be3eb54
MONICAPRO0607 <monicapro0607@gmail.com>
2025-11-16 17:57:32 +0100

    deploy

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import './Navbar.css'
+import './NavBar.css'
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false)

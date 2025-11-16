fa89b8be0d49a52f64b56624bbf6a00a2746b6e9
MONICAPRO0607 <monicapro0607@gmail.com>
2025-11-16 18:33:13 +0100

    deploy

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -22,7 +22,7 @@ const AdminPanel = () => {
     setError("");
 
     try {
-      const res = await fetch(`${API_URL}/auth/login`, {
+      const res = await fetch(`${API_URL}/auth`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
